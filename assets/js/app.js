392374948096676722b5ce24885a005a34298477
Natali Maximenko <natali.maximenko@gmail.com>
2019-02-14 15:52:05 +0300

    Fix webpack settings

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import css from "../css/app.css"
+import 'bootstrap'
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -15,3 +16,7 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
